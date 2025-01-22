63d13d2320d45d6fb9c7398c2f9cbf986672e0f5
wzhua02 <woozongghua@gmail.com>
2025-01-22 15:43:49 +0800

    Done Level-4

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -6,10 +6,29 @@ public class Baymax {
     private static class Task {
         protected String description;
         protected boolean isDone;
+        protected String type;
+        protected String toDate;
+        protected String fromDate;
 
-        public Task(String description) {
+        public Task(String description, String type) {
             this.description = description;
             this.isDone = false;
+            this.type = "T";
+        }
+
+        public Task(String description, String type, String toDate) {
+            this.description = description;
+            this.isDone = false;
+            this.type = "D";
+            this.toDate = toDate;
+        }
+
+        public Task(String description, String type, String fromDate, String toDate) {
+            this.description = description;
+            this.isDone = false;
+            this.type = "E";
+            this.toDate = toDate;
+            this.fromDate = fromDate;
         }
 
         public String getStatusIcon() {
@@ -20,9 +39,25 @@ public class Baymax {
             this.isDone = mark;
         }
 
+        public String getToDate() {
+            return toDate;
+        }
+
+        public String getFromDate() {
+            return fromDate;
+        }
+
         @Override
         public String toString() {
-            return description;
+            String typeStr = "[" + this.type + "]";
+            String markStr = "[" + this.getStatusIcon() + "] ";
+            String retString = typeStr + markStr + description;
+            if (this.type.equals("D")) {
+                retString += " (by: " + toDate + ")";
+            } else if (this.type.equals("E")) {
+                retString += " (from: " + fromDate + " to: " + toDate + ")";
+            }
+            return retString;
         }
     }
     private static void reply(String msg, String... otherMsgs) {
@@ -48,21 +83,49 @@ public class Baymax {
                 ArrayList<String> allTaskArray = new ArrayList<>();
                 for (int i = 0; i < taskList.size(); i++) {
                     Task task = taskList.get(i);
-                    allTaskArray.add((i + 1) + ". [" + task.getStatusIcon() + "] " + task);
+                    allTaskArray.add((i + 1) + ". " + task);
                 }
                 reply("Here are your tasks: ", allTaskArray.toArray(new String[0]));
-            } else if (cmd.equals("mark")) {
+            } else if (cmd.equals("mark") || cmd.equals("unmark")) {
                 int idx = Integer.parseInt(input.split(" ")[1]) - 1;
                 Task theTask = taskList.get(idx);
-                theTask.marker(true);
-                reply("Okie this is marked as done: ", "   [" + theTask.getStatusIcon() + " ] " + theTask);
-            } else if (cmd.equals("unmark")) {
-                int idx = Integer.parseInt(input.split(" ")[1]) - 1;
-                Task theTask = taskList.get(idx);
-                theTask.marker(false);
-                reply("Okie this is marked as not done yet: ", "   [" + theTask.getStatusIcon() + "] " + theTask);
+                theTask.marker(cmd.equals("mark"));
+                String markMsg = cmd.equals("mark")
+                        ? "Okie dokay this is marked as done: "
+                        : "Okie this is marked as not done yet: ";
+                reply(markMsg, "   " + theTask);
+            } else if (cmd.equals("todo")) {
+                int spaceIdx = input.indexOf(" ");
+                String taskDescribe = input.substring(spaceIdx + 1);
+                Task newTask = new Task(taskDescribe, cmd);
+                taskList.add(newTask);
+                reply("Got it. Added this task: ",
+                        newTask.toString(),
+                        "Now you have " + taskList.size() + " tasks in the list. ");
+            } else if (cmd.equals("deadline")) {
+                int spaceIdx = input.indexOf(" ");
+                int byIdx = input.indexOf("/by");
+                String taskDescribe = input.substring(spaceIdx + 1, byIdx - 1);
+                String deadlineDate = input.substring(byIdx + 4);
+                Task newTask = new Task(taskDescribe, cmd, deadlineDate);
+                taskList.add(newTask);
+                reply("Got it. Added this task: ",
+                        newTask.toString(),
+                        "Now you have " + taskList.size() + " tasks in the list. ");
+            } else if (cmd.equals("event")) {
+                int spaceIdx = input.indexOf(" ");
+                int fromIdx = input.indexOf("/from");
+                int toIdx = input.indexOf("/to");
+                String taskDescribe = input.substring(spaceIdx + 1, fromIdx - 1);
+                String fromDate = input.substring(fromIdx + 6, toIdx - 1);
+                String toDate = input.substring(toIdx + 4);
+                Task newTask = new Task(taskDescribe, cmd, fromDate, toDate);
+                taskList.add(newTask);
+                reply("Got it. Added this task: ",
+                        newTask.toString(),
+                        "Now you have " + taskList.size() + " tasks in the list. ");
             } else {
-                taskList.add(new Task(input));
+                taskList.add(new Task(input, null));
                 reply("added " + input);
             }
             input = scan.nextLine();
