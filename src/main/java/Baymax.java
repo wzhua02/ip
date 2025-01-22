64989d4ff12e33071ddeba6eca524948968b29ec
wzhua02 <woozongghua@gmail.com>
2025-01-22 14:19:01 +0800

    Converted tasks from just Strings to the class Task; input is now split to String[], taskList uses ArrayList to store

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,7 +1,26 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Baymax {
+    private static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+        @Override
+        public String toString() {
+            return description;
+        }
+    }
     private static void reply(String msg, String... otherMsgs) {
         String indent = "    ";
         String horizontal_line = indent + "_".repeat(50);
@@ -14,22 +33,29 @@ public class Baymax {
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String[] taskList = new String[100];
-        int taskCount = 0;
+        ArrayList<Task> taskList = new ArrayList<>();
 
         //Chatbot function
         reply("Hello! I'm Baymax", "What can I do for you?");
-        String input = scan.nextLine();
-        while (!input.equals("bye")) {
-            if (input.equals("list")) {
-                String[] allTasks = Arrays.copyOf(taskList, taskCount);
-                reply("Here are your tasks: ", allTasks);
-            } else {
-                taskList[taskCount] = (taskCount + 1) + ". " + input;
-                taskCount++;
-                reply("added " + input);
+        String[] input = scan.nextLine().split(" ");
+        while (!input[0].equals("bye")) {
+            switch(input[0]) {
+                case "list":
+                    ArrayList<String> allTaskArray = new ArrayList<>();
+                    for (int i = 0; i < taskList.size(); i++) {
+                        Task task = taskList.get(i);
+                        allTaskArray.add((i + 1) + ". [" + task.getStatusIcon() + "] " + task);
+                    }
+                    reply("Here are your tasks: ", allTaskArray.toArray(new String[0]));
+                case "mark":
+
+                case "unmark":
+
+                default:
+                    taskList.add(new Task(input[0]));
+                    reply("added " + input[0]);
             }
-            input = scan.nextLine();
+            input = scan.nextLine().split(" ");
         }
         reply("Bye. Hope to see you again soon!");
     }
