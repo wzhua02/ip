53d7b1291dab7d7905537f6808da15ec9aa8bf75
wzhua02 <woozongghua@gmail.com>
2025-01-22 11:47:21 +0800

    Renamed Duke to Baymax

---
+++ src/main/java/Baymax.java
@@ -0,0 +1,10 @@
+public class Duke {
+    public static void main(String[] args) {
+        String horizontal_line = "_".repeat(50);
+        System.out.println("Hello! I'm Baymax");
+        System.out.println("What can I do for you?");
+        System.out.println(horizontal_line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal_line);
+    }
+}
