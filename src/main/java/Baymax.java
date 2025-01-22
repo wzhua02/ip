74bb589de0fea65b90acdb94c71db9f73837c67b
wzhua02 <woozongghua@gmail.com>
2025-01-22 14:39:25 +0800

    Done level 3; Change switch-case to simply if-else

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -16,6 +16,10 @@ public class Baymax {
             return (isDone ? "X" : " "); // mark done task with X
         }
 
+        public void marker(boolean mark) {
+            this.isDone = mark;
+        }
+
         @Override
         public String toString() {
             return description;
@@ -37,25 +41,32 @@ public class Baymax {
 
         //Chatbot function
         reply("Hello! I'm Baymax", "What can I do for you?");
-        String[] input = scan.nextLine().split(" ");
-        while (!input[0].equals("bye")) {
-            switch(input[0]) {
-                case "list":
-                    ArrayList<String> allTaskArray = new ArrayList<>();
-                    for (int i = 0; i < taskList.size(); i++) {
-                        Task task = taskList.get(i);
-                        allTaskArray.add((i + 1) + ". [" + task.getStatusIcon() + "] " + task);
-                    }
-                    reply("Here are your tasks: ", allTaskArray.toArray(new String[0]));
-                case "mark":
-
-                case "unmark":
-
-                default:
-                    taskList.add(new Task(input[0]));
-                    reply("added " + input[0]);
+        String input = scan.nextLine();
+        String cmd = input.split(" ")[0];
+        while (!cmd.equals("bye")) {
+            if (cmd.equals("list")) {
+                ArrayList<String> allTaskArray = new ArrayList<>();
+                for (int i = 0; i < taskList.size(); i++) {
+                    Task task = taskList.get(i);
+                    allTaskArray.add((i + 1) + ". [" + task.getStatusIcon() + "] " + task);
+                }
+                reply("Here are your tasks: ", allTaskArray.toArray(new String[0]));
+            } else if (cmd.equals("mark")) {
+                int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task theTask = taskList.get(idx);
+                theTask.marker(true);
+                reply("Okie this is marked as done: ", "   [" + theTask.getStatusIcon() + " ] " + theTask);
+            } else if (cmd.equals("unmark")) {
+                int idx = Integer.parseInt(input.split(" ")[1]) - 1;
+                Task theTask = taskList.get(idx);
+                theTask.marker(false);
+                reply("Okie this is marked as not done yet: ", "   [" + theTask.getStatusIcon() + "] " + theTask);
+            } else {
+                taskList.add(new Task(input));
+                reply("added " + input);
             }
-            input = scan.nextLine().split(" ");
+            input = scan.nextLine();
+            cmd = input.split(" ")[0];
         }
         reply("Bye. Hope to see you again soon!");
     }
