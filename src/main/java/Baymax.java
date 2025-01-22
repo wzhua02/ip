1158ed9004c436d940a2411a96cab9fc06c7d478
wzhua02 <woozongghua@gmail.com>
2025-01-22 12:30:18 +0800

    Did Level 1: Echo and Level 2: Add, List

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,10 +1,36 @@
-public class Duke {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Baymax {
+    private static void reply(String msg, String... otherMsgs) {
+        String indent = "    ";
+        String horizontal_line = indent + "_".repeat(50);
+        System.out.println(horizontal_line);
+        System.out.println(indent + msg);
+        for (String msgs : otherMsgs) {
+            System.out.println(indent + msgs);
+        }
+        System.out.println(horizontal_line);
+    }
     public static void main(String[] args) {
-        String horizontal_line = "_".repeat(50);
-        System.out.println("Hello! I'm Baymax");
-        System.out.println("What can I do for you?");
-        System.out.println(horizontal_line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontal_line);
+        Scanner scan = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int taskCount = 0;
+
+        //Chatbot function
+        reply("Hello! I'm Baymax", "What can I do for you?");
+        String input = scan.nextLine();
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                String[] allTasks = Arrays.copyOf(taskList, taskCount);
+                reply("Here are your tasks: ", allTasks);
+            } else {
+                taskList[taskCount] = (taskCount + 1) + ". " + input;
+                taskCount++;
+                reply("added " + input);
+            }
+            input = scan.nextLine();
+        }
+        reply("Bye. Hope to see you again soon!");
     }
 }
