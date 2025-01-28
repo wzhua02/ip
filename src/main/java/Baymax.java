3502ba9793d1282354f548dde4035967c7444baf
wzhua02 <woozongghua@gmail.com>
2025-01-28 22:38:11 +0800

    Modify functionality of Parser.java
    Moved while loop to Baymax.java.
    
    Moved parseDateTime function to Parser.java.
    
    Renamed UserInterface to Ui.

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,25 +1,24 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Baymax {
     private static final Path FILE_PATH = Paths.get("data", "tasks.txt");
     private final Storage storage;
     private final TaskList tasks;
-    private final UserInterface ui;
 
     public Baymax(Path filePath) {
-        ui = new UserInterface();
         storage = new Storage(filePath);
         tasks = new TaskList(storage.load());
     }
 
     public void run() {
-        ui.reply("Hello! I'm Baymax", "How can I assist you?");
-        String input = ui.getInput();
-        Parser.parse(input, ui, tasks, storage);
-        ui.reply("Goodbye! *slowly deflates*");
+        Ui.reply("Hello! I'm Baymax", "How can I assist you?");
+        String input = Ui.getInput();
+        while (!input.startsWith("bye")) {
+            Parser.parse(input, tasks, storage);
+            input = Ui.getInput();
+        }
+        Ui.reply("Goodbye! *slowly deflates*");
     }
 
     public static void main(String[] args) {
